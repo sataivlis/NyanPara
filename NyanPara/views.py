7b5357147227404aa9c52a8d521c8004c59808df
SataIvlis <yjie@ucmerced.edu>
2019-07-21 13:37:10 -0700

    register->back

--- NyanPara/views.py
+++ NyanPara/views.py
@@ -8,19 +8,21 @@ def my_login(request):
     # from login.html. The values in this request.POST dictionary are given from
     # homepage, but with a logged-in template.
     # the <input ... name="______">
-    username = request.POST['username']
-    password = request.POST['password']
+    try:
+        username = request.POST['username']
+        password = request.POST['password']
+    except:
+        return render(request, 'login.html')
 
     # check to see if the person actually typed in values for username and password
-    if username and password:
-        print("There's a username and password!")
-        print(username, password)
-        user = authenticate(username=username, password=password)
-        print(user)
-        if user:
-            print("Redirecting to nyannyan!")
-            login(request, user)
-            return redirect('nyannyan')
+    print("There's a username and password!")
+    print(username, password)
+    user = authenticate(username=username, password=password)
+    print(user)
+    if user:
+        print("Redirecting to nyannyan!")
+        login(request, user)
+        return redirect('nyannyan')
 
     return render(request, 'login.html', {'fail': True})
 
@@ -35,24 +37,28 @@ def register(request):
     return render(request, 'register.html', {})
 
 # After hitting the "Register" Button, create a User instance into the DB,
+# 1. Get the values from the request dictionary (from HTML)
 # authenticate the new user, and return to the homepage.
 def register_user(request):
 
-    first_name = request.POST['first_name']
-    last_name = request.POST['last_name']
-    password = request.POST['password']
-    username = request.POST['username']
-
-    # 2. Create a user with that information like we did in the shell
-    u = User(username=username, first_name=first_name, last_name=last_name)
-    u.set_password(password)
-
     try:
-        u.save()
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password = request.POST['password']
+        username = request.POST['username']
     except:
-        # What should we do in the case where there was an error with registration?
         return render(request, 'register.html', {'fail': True})
 
+    # 2. Create a user with that information like we did in the shell
+        user = User(username=username, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+
+        try:
+            user.save()
+        except:
+            # What should we do in the case where there was an error with registration?
+            return render(request, 'register.html', {'fail': True})
+
     # 3. authenticate the user, then redirect to the homepage.
-    login(request, u)
-    return redirect('nyannyan')
+        login(request, user)
+        return redirect('nyannyan')
