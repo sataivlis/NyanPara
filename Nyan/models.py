180fab544ef64bb8d27f668ec76fd91a0d7001db
SataIvlis <yjie@ucmerced.edu>
2019-07-21 15:08:15 -0700

    modified Cat and added UserCat table

--- Nyan/models.py
+++ Nyan/models.py
@@ -3,9 +3,18 @@ from django.conf import settings
 from django.utils import timezone
 
 class Cat(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    birth_date = models.DateTimeField(default=timezone.now) #is timezone.now a function? not too sure
+    breed = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    img_url = models.CharField(max_length=300)
 
     def __str__(self):
-        return self.name
+        return self.color + " " + self.breed
+
+class UserCat(models.Model):
+    cat_id = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=100)
+    birthdate = models.DateTimeField(default=timezone.now) #is timezone.now a function? not too sure
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nickname + "belonging to " + owner.name
